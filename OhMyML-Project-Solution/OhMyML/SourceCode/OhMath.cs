e32384cf3fae40537ebca1e8ff209092d207b46d
dylan <dalexander@carterandclark.com>
2021-05-18 14:00:13 -0400

    Added EuclideanDistance() to OhMath.cs
    Also made OhMath.cs an internal static class, much like Tools.cs in dynamicad

--- OhMyML-Project-Solution/OhMyML/SourceCode/OhMath.cs
+++ OhMyML-Project-Solution/OhMyML/SourceCode/OhMath.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 namespace OhMyML.SourceCode
 {
-    public class OhMath
+    internal static class OhMath
     {
-        public OhMath()
+        public static double EuclideanDistance(double[] sampleOne, double[] sampleTwo)
         {
+            double d = 0.0;
+
+            for(int i = 0; i < sampleOne.Length; i++)
+            {
+                double temp = sampleOne[i] - sampleTwo[i];
+                d += temp * temp;
+            }
+
+            return Math.Sqrt(d);
         }
     }
 }
