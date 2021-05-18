eba5c91d1b65ca190a3a63abf5a3bdad3dc137ce
Zach Vanscoit <zachary816@gmail.com>
2021-05-18 15:31:50 -0400

    example test

--- OhMyML-Project-Solution/OhMyML/SourceCode/OhMyML_Runnable.cs
+++ OhMyML-Project-Solution/OhMyML/SourceCode/OhMyML_Runnable.cs
@@ -10,8 +10,15 @@ namespace OhMyML
 	{
 		static void Main(string[] args)
 		{
-			SupervisedLearning<float[], LinearRegressor, float[]> sl;
-			sl = new SupervisedLearning<float[], LinearRegressor, float[]>();
+			float[] data = new float[] {3.4f, 29.43f};
+			SupervisedLearning<float[], LinearRegressor, float[]> sl = new SupervisedLearning<float[], LinearRegressor, float[]>();
+			sl.SetInput(data);
+			float[] result = sl.GetOutput();
+
+			foreach (float r in result)
+			{
+				Console.WriteLine($"Our result was: {r}\n");
+			}
 		}
 	}
 }
