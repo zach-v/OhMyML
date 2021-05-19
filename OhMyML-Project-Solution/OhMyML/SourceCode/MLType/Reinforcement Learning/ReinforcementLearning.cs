65fec157cceac2e1a2cbef436de8721d584e463f
Zach Vanscoit <zachary816@gmail.com>
2021-05-19 11:24:28 -0400

    removed more confusing stuff
    cause we just recently polished our brains, so we can't follow our original complex ideas.

--- OhMyML-Project-Solution/OhMyML/SourceCode/MLType/Reinforcement Learning/ReinforcementLearning.cs
+++ OhMyML-Project-Solution/OhMyML/SourceCode/MLType/Reinforcement Learning/ReinforcementLearning.cs
@@ -1,20 +1,10 @@
 ﻿using System;
 namespace OhMyML.SourceCode.MLType
 {
-    public class ReinforcementLearning<I, L, O> : MLTypeInterface<I, L, O>
+    public class ReinforcementLearning
     {
         public ReinforcementLearning()
         {
         }
-
-		public O GetOutput()
-		{
-			throw new NotImplementedException();
-		}
-
-		public void SetInput(I input)
-		{
-			throw new NotImplementedException();
-		}
-	}
+    }
 }
